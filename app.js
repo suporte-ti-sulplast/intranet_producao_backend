824b94d33755f7ef784d7eca0a467f303850eaf8
suporte-ti-sulplast <suporte.ti@sulplast.com.br>
2024-06-26 14:10:04 +0000

    add arquivo do webserver arduino

--- app.js
+++ app.js
@@ -1,55 +1,56 @@
-const express = require('express');
-const cors = require("cors");
+const express = require("express");
 const app = express();
-const path = require('path');
-const router = require("./src/routes/_index"); 
-const multer = require('multer');
-const ldapFunctions = require('./src/conections/ldap');
-require('dotenv').config();
-const { getSensorDataRackSalaTI } = require('./src/conections/arduino');
-const { monitorSensorDataRackSalaTI } = require('./src/functions/getDataArduinos');
+const path = require("path");
+const router = require("./src/routes/_index");
+const ldapFunctions = require("./src/conections/ldap");
+require("dotenv").config();
+const { getSensorDataRackSalaTI } = require("./src/conections/arduino");
+const {
+  monitorSensorDataRackSalaTI,
+} = require("./src/functions/getDataArduinos");
 
-const { binpack } = require('./src/functions/binpack')
+const { binpack } = require("./src/functions/binpack");
 
 //configura o CORs
 app.use((req, res, next) => {
-  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
   const origin = req.headers.origin;
-
 
   // Verifica se a origem da solicitação está na lista de origens permitidas
   if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Origin", origin);
   }
 
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization');
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-PINGOTHER, Content-Type, Authorization"
+  );
   next();
 });
 
-const upload = require('./src/routes/_upload');
-const authRouter =  require('./src/routes/_auth');
-const indexRouter =  require('./src/routes/_index');
-const UsuariosRoutes = require('./src/routes/UsuariosRoutes');
-const NoticiasRoutes = require('./src/routes/NoticiasRoutes');
-const EtiquetasRoutes = require('./src/routes/EtiquetasRoutes');
-const ImpressorasRoutes = require('./src/routes/ImpressorasRoutes');
-const DepartamentosRoutes = require('./src/routes/DepartamentosRoutes');
-const SenhasCofreRoutes = require('./src/routes/SenhasCofreRoutes');
-const RelatoriosRoutes = require('./src/routes/RelatoriosRoutes');
-const PortariaRoutes = require('./src/routes/PortariaRoutes');
-const ArquivosRouter = require('./src/routes/ArquivosRouter'); 
-const CoqRoutes = require('./src/routes/CoqRoutes'); 
-const PaineisRoutes = require('./src/routes/PaineisRoutes'); 
-const RecepcaoRouters = require('./src/routes/RecepcaoRoutes'); 
-const PcpRouters = require('./src/routes/PcpRoutes'); 
-const MonitoresRouter = require('./src/routes/MonitoresRouter'); 
-const DelsoftRouter = require('./src/routes/DelsoftRoutes'); 
-const LogsRouter = require('./src/routes/LogsRoutes'); 
-const TelasRouter = require('./src/routes/TelasRoutes'); 
+const upload = require("./src/routes/_upload");
+const authRouter = require("./src/routes/_auth");
+const indexRouter = require("./src/routes/_index");
+const UsuariosRoutes = require("./src/routes/UsuariosRoutes");
+const NoticiasRoutes = require("./src/routes/NoticiasRoutes");
+const EtiquetasRoutes = require("./src/routes/EtiquetasRoutes");
+const ImpressorasRoutes = require("./src/routes/ImpressorasRoutes");
+const DepartamentosRoutes = require("./src/routes/DepartamentosRoutes");
+const SenhasCofreRoutes = require("./src/routes/SenhasCofreRoutes");
+const RelatoriosRoutes = require("./src/routes/RelatoriosRoutes");
+const PortariaRoutes = require("./src/routes/PortariaRoutes");
+const ArquivosRouter = require("./src/routes/ArquivosRouter");
+const CoqRoutes = require("./src/routes/CoqRoutes");
+const PaineisRoutes = require("./src/routes/PaineisRoutes");
+const RecepcaoRouters = require("./src/routes/RecepcaoRoutes");
+const PcpRouters = require("./src/routes/PcpRoutes");
+const MonitoresRouter = require("./src/routes/MonitoresRouter");
+const DelsoftRouter = require("./src/routes/DelsoftRoutes");
+const LogsRouter = require("./src/routes/LogsRoutes");
+const TelasRouter = require("./src/routes/TelasRoutes");
 
-
-const { rackSalaTI } = require('./src/services//sendEmailSensors');
+const { rackSalaTI } = require("./src/services//sendEmailSensors");
 
 app.use(router);
 
@@ -57,7 +58,7 @@ app.use(express.json()); // Para analisar JSON
 app.use(express.urlencoded({ extended: true })); // Para analisar dados de formulários
 
 //CAMINHO DA PASTA PUBLIC - JS, CSS, IMAGENS
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //ROTAS
 app.use("/auth", authRouter);
@@ -82,18 +83,40 @@ app.use("/", LogsRouter);
 app.use("/", TelasRouter);
 
 // Configurar uma rota para servir imagens
-app.use('/images-vehicles', express.static(path.join(__dirname, 'public/upload/vehicles')));
+app.use(
+  "/images-vehicles",
+  express.static(path.join(__dirname, "public/upload/vehicles"))
+);
 //rota para fazer upload de imagem
-app.use('/upload', upload);
+app.use("/upload", upload);
 
 // Endpoint para servir arquivos
-app.use('/files-lgpd', express.static(path.join(__dirname, 'public/sharedFiles/lgpd')));
-app.use('/files-manuais-informatica', express.static(path.join(__dirname, 'public/sharedFiles/manuaisinformatica')));
-app.use('/files-arquivos-downloads', express.static(path.join(__dirname, 'public/sharedFiles/arquivosdownloads')));
-app.use('/files-its', express.static(path.join(__dirname, 'public/sharedFiles/sgi/its')));
-app.use('/files-comunicacoes', express.static(path.join(__dirname, 'public/sharedFiles/comunicacoes')));
-app.use('/files-noticias', express.static(path.join(__dirname, 'public/sharedFiles/comunicacoes/noticias')));
-
+app.use(
+  "/files-lgpd",
+  express.static(path.join(__dirname, "public/sharedFiles/lgpd"))
+);
+app.use(
+  "/files-manuais-informatica",
+  express.static(path.join(__dirname, "public/sharedFiles/manuaisinformatica"))
+);
+app.use(
+  "/files-arquivos-downloads",
+  express.static(path.join(__dirname, "public/sharedFiles/arquivosdownloads"))
+);
+app.use(
+  "/files-its",
+  express.static(path.join(__dirname, "public/sharedFiles/sgi/its"))
+);
+app.use(
+  "/files-comunicacoes",
+  express.static(path.join(__dirname, "public/sharedFiles/comunicacoes"))
+);
+app.use(
+  "/files-noticias",
+  express.static(
+    path.join(__dirname, "public/sharedFiles/comunicacoes/noticias")
+  )
+);
 
 /* ldapFunctions.buscarUsuarios((err, usuarios) => {
   if (err) {
@@ -105,12 +128,14 @@ app.use('/files-noticias', express.static(path.join(__dirname, 'public/sharedFil
 
 /* binpack(); */
 
+//cria a conxão com o servidor do arduíno
 getSensorDataRackSalaTI();
+//faz a requisição dos dados dos sensores a cada 60 segundos
 monitorSensorDataRackSalaTI(60);
-// Inicia serviços a cada 20 segundos
-rackSalaTI(60*10);
+//Verifica os dados recebidos do sensor a cada 10 segundos para disparo de notificações de alerta por email
+rackSalaTI(60 * 10);
 
 //inicializa o servidor
 app.listen(process.env.PORT, () => {
-  console.log('Servidor rodando na porta:', process.env.PORT);
+  console.log("Servidor rodando na porta:", process.env.PORT);
 });
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,35 @@
 module.exports = {
   apps: [
     {
-      name: 'backend-DEV',
-      script: '/var/www/backend/app.js',
-      cwd: '/var/www/backend',
+      name: "intranet-backend",
+      script: "/var/www/backend/app.js",
+      cwd: "/var/www/backend",
       instances: 2,
-      exec_mode: 'cluster',
+      exec_mode: "cluster",
       watch: false,
-      max_memory_restart: '200M',
+      max_memory_restart: "500M",
       // ... outras configurações ...
     },
     {
-      name: 'frontend-DEV',
-      script: 'npm',
-      args: 'start',
-      interpreter: 'none',
-      cwd: '/var/www/frontend',
-      watch: true,
-      max_memory_restart: '200M',
+      name: "intranet-frontend",
+      script: "npm",
+      args: "start",
+      interpreter: "none",
+      cwd: "/var/www/frontend",
+      watch: false,
+      max_memory_restart: "500M",
       // ... outras configurações ...
     },
   ],
+  // Configurações globais
+  deploy: {
+    production: {
+      // Configurações de produção
+    },
+    development: {
+      // Configurações de desenvolvimento
+    },
+  },
   // Configurações adicionais
   delay: 10000,
 };
